b8fbea06dd2de589a42647dc8facc85ecb8d9ab6
MartinaFalco <03mleguizamon@gmail.com>
2024-11-09 17:34:55 -0300

    Solucionado error de conexión a Mongo

--- src/db/mongo.db.js
+++ src/db/mongo.db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose")
 //Nombre de la base de datos: productosDB
-const MONGO_URL = process.env.MONGO_URL || "mongodb://admin:admin1234@localhost:27017/productosDB?authSource=admin"
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/productosDB?authSource=admin"
 
 async function connectToDatabase() {
   try {
     await mongoose.connect(MONGO_URL);
     console.log("Conexión a mongo realizada con exito");
   } catch (err) {
-    console.error("Error al conectarse a mongo", err.message);
+    console.error("Error al conectarse a mongo", err);
   }
 }
 
