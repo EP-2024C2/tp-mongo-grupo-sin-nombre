b915d5c6a71e614dc1a645c6e3ecd964d880205e
MartinaFalco <03mleguizamon@gmail.com>
2024-11-13 00:32:50 -0300

    Agregados schemas de fabricante y rutas

--- src/routes/fabricanteRoute.js
+++ src/routes/fabricanteRoute.js
@@ -1,14 +1,18 @@
 const { Router } = require('express')
 const fabricanteController = require('../controllers/fabricanteController')
+const Fabricante = require('../models/fabricanteModel');
+const fabricanteMiddleware = require('../middlewares/middleware');
+const fabricanteSchema = require('../schemas/fabricanteSchema')
 const fabricanteRoute = Router()
 
 fabricanteRoute.get('/fabricantes', fabricanteController.getFabricantes);
-fabricanteRoute.get('/fabricantes/:id', fabricanteController.getFabricantesId)
-fabricanteRoute.post('/fabricantes', fabricanteController.createFabricante)
-fabricanteRoute.put('/fabricantes/:id', fabricanteController.updateFabricante)
-fabricanteRoute.delete('/fabricantes/:id',fabricanteController.deleteFabricante)
+fabricanteRoute.get('/fabricantes/:id', fabricanteMiddleware.existsById(Fabricante), fabricanteController.getFabricantesId);
+fabricanteRoute.post('/fabricantes', fabricanteMiddleware.validateSchema(fabricanteSchema), fabricanteController.createFabricante); 
+fabricanteRoute.put('/fabricantes/:id', fabricanteMiddleware.existsById(Fabricante), fabricanteMiddleware.validateSchema(fabricanteSchema), fabricanteController.updateFabricante)
+fabricanteRoute.delete('/fabricantes/:id', fabricanteMiddleware.existsById(Fabricante), fabricanteController.deleteFabricante);
 
-fabricanteRoute.get('/fabricantes/:id/productos', fabricanteController.getProductosInFabricante)
+
+fabricanteRoute.get('/fabricantes/:id/productos',fabricanteMiddleware.existsById(Fabricante), fabricanteController.getProductosInFabricante)
 
 
 module.exports = fabricanteRoute
---
+++ src/schemas/fabricanteSchema.js
@@ -0,0 +1,35 @@
+const Joi = require("joi")
+
+const fabricanteSchema = Joi.object().keys({
+
+    nombre: Joi.string().required().messages({
+        "any.required": "El campo nombre es obligatorio",
+        "string.empty": "El campo nombre no puede estar vacio"  
+
+    }),
+
+    direccion: Joi.string().required().messages({
+        "any.required": "El campo direccion es obligatorio",
+        "string.empty": "El campo direccion no puede estar vacio"  
+
+    }),
+
+    numeroContacto: Joi.number().required().messages({
+        "any.required": "El campo numeroContacto es obligatorio",
+        "number.integer": "El campo numeroContacto debe ser un número entero",
+
+    }),
+    pathIMG: Joi.string().required().messages({
+        "any.required": "El campo pathImgPerfil es obligatorio",
+        "string.empty": "El campo pathImgPerfil no puede estar vacio"  
+
+    }),
+    productoId: Joi.any().messages({ 
+        "any.required": "El campo pathImgPerfil es obligatorio",
+        "string.empty": "El campo pathImgPerfil no puede estar vacio"  
+
+    })
+
+})
+
+module.exports = fabricanteSchema
